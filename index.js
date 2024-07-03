304b5824bbe7fdbdf8f7cda82cbbf12ee4d6024a
Xliyy <167864452+Xliyy@users.noreply.github.com>
2024-07-03 10:43:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,26 @@
 
-const formEl = document.querySelector(".form");
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const amineNameEl = document.querySelector(".anime-name");
 
-const inputEl = document.querySelector(".input");
-
-const ulEl = document.querySelector(".list");
-
-let list = JSON.parse(localStorage.getItem("list"));
-if (list) {
-  list.forEach((task) => {
-    toDoList(task);
-  });
-}
-
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  toDoList();
+btnEl.addEventListener("click", async function () {
+  try {
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    amineNameEl.innerText = "Updating...";
+    animeImgEl.src = "spinner.svg";
+    const response = await fetch("https://api.catboys.com/img");
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    amineNameEl.innerText = data.artist;
+  } catch (error) {
+    console.log(error);
+    btnEl.disabled = false;
+    btnEl.innerText = "Get Anime";
+    amineNameEl.innerText = "An error happened, please try again";
+  }
 });
-
-function toDoList(task) {
-  let newTask = inputEl.value;
-  if (task) {
-    newTask = task.name;
-  }
-
-  const liEl = document.createElement("li");
-  if (task && task.checked) {
-    liEl.classList.add("checked");
-  }
-  liEl.innerText = newTask;
-  ulEl.appendChild(liEl);
-  inputEl.value = "";
-  const checkBtnEl = document.createElement("div");
-  checkBtnEl.innerHTML = `
-  <i class="fas fa-check-square">
-  `;
-  liEl.appendChild(checkBtnEl);
-  const trashBtnEl = document.createElement("div");
-  trashBtnEl.innerHTML = `
-  <i class="fas fa-trash"></i>
-  `;
-  liEl.appendChild(trashBtnEl);
-
-  checkBtnEl.addEventListener("click", () => {
-    liEl.classList.toggle("checked");
-    updateLocalStorage();
-  });
-
-  trashBtnEl.addEventListener("click", () => {
-    liEl.remove();
-    updateLocalStorage();
-  });
-  updateLocalStorage();
-}
-
-function updateLocalStorage() {
-  const liEls = document.querySelectorAll("li");
-  list = [];
-  liEls.forEach((liEl) => {
-    list.push({
-      name: liEl.innerText,
-      checked: liEl.classList.contains("checked"),
-    });
-  });
-  localStorage.setItem("list", JSON.stringify(list));
-}
